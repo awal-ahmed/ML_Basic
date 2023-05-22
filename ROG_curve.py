18532bd5693ea785696424990cc2173b89560ef8
awal-ahmed <awalahmedfime984@gmail.com>
2023-05-23 01:15:26 +0600

    Added coded for multiclass roc curve generation

---
+++ ROG_curve.py
@@ -0,0 +1,58 @@
+# For multi class the rog curve generation code is following
+
+# n_classes = the number of classes
+# y_test = the one hotted version of actual class label
+# y_score = the one hotted version of predicted class label
+
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+n_classes = 4   # the number of classes
+
+# If your data is not one hotted then use this line of code 
+y_test = preprocessing.label_binarize(y_test, classes=range(n_classes))
+y_score = preprocessing.label_binarize(y_score, classes=range(n_classes))
+
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+# Plot of a ROC curve for a specific class
+plt.figure()
+plt.plot(fpr[2], tpr[2], label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
+
+# Plot ROC curve
+plt.figure()
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]))
+for i in range(n_classes):
+    plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
+                                   ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right")
+plt.show()
